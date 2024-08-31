cecca69efcd3d99da77f98fd7714772ba8f0a155
markitos7783 <159593718+markitos7783@users.noreply.github.com>
2024-08-30 18:41:08 -0700

    Add files via upload

---
+++ BasedeDatosWeb/Parcial1/hola mundo.php
@@ -0,0 +1,15 @@
+<?php
+ $nombre = "marco";
+ $numero = 4;
+ $numero2 ="4";
+ $total = $numero + $numero2;
+
+ echo "total =" .$total;
+ echo "<h1>HOLA MUNDO</H1>";
+ for ($i=0; $i < 10; $i++) { 
+    echo $i . "<br>";
+
+ }
+ echo "marco antonio larios villaseñor"
+ 
+?>
